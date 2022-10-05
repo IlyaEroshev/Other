624ca34a6c7fc4a0e3d22d4c9cae87b91c5f3eda
Eroshev Ilya <90902652+IlyaEroshev@users.noreply.github.com>
2022-10-05 20:19:29 +0300

    Update M.h

--- Modular arithmetic/M.h
+++ Modular arithmetic/M.h
@@ -1,96 +1,93 @@
-#include <iostream>
-#include <vector>
-
 template <uint32_t mod>
-class M{//uint64_t можно заменить на __int128
+class ModularArithmetic{//uint64_t можно заменить на __int128
 	uint64_t value;
 public:
-	M(): value(0ull) {}
+	inline ModularArithmetic(): value(0ull) {}
 
-	M(long long value) {
+	inline ModularArithmetic(long long value) {
 		value %= mod;
 		if(value < 0) value += mod;
 		this->value = value;
 	}
 
-	uint32_t get_mod() const {
+	inline uint32_t get_mod() const {
 		return mod;
 	}
 
-	uint64_t get_value() const {
+	inline uint64_t get_value() const {
 		return value;
 	}
 
-	M& operator+=(const M &a) {
+	inline ModularArithmetic& operator+=(const ModularArithmetic &a) {
 		value += a.value;
 		value %= mod;
 		return *this;
 	}
 
-	M& operator-=(const M &a) {
+	inline ModularArithmetic& operator-=(const ModularArithmetic &a) {
 		value += mod - a.value;
 		value %= mod;
 		return *this;
 	}
 
-	M& operator*=(const M &a) {
+	inline ModularArithmetic& operator*=(const ModularArithmetic &a) {
 		value *= a.value;
 		value %= mod;
 		return *this;
 	}
 
-	M& operator/=(const M &a) {
-		M inv_a = pow(a, mod - 2);
+	inline ModularArithmetic& operator/=(const ModularArithmetic &a) {
+		ModularArithmetic inv_a = pow(a, mod - 2);
 		return *this *= inv_a;
 	}
 
-	inline static M F(long n) {//факториал n
-		static std::vector<M> fucts(2, 1);
-		if(fucts.size() < n)
-			fucts.push_back(F(n-1));
+	inline static ModularArithmetic F(long n) {//факториал n
+		static std::vector<ModularArithmetic> fucts(2, 1);
+		if(fucts.size() <= n)
+			fucts.push_back(F(n-1) * ModularArithmetic(n));
 		return fucts[n];
 	}
 
-	inline static M A(long n, long k) {//кол-во размещений из n по k
+	inline static ModularArithmetic A(long n, long k) {//кол-во размещений из n по k
 		return F(n) / F(n-k);
 	}
 
-	inline static M C(long n, long k) {//кол-во сочетаний из n по k
+	inline static ModularArithmetic C(long n, long k) {//кол-во сочетаний из n по k
 		return F(n) / (F(k) * F(n-k));
 	}
 };
 
 template <uint32_t mod>
-M<mod> operator+(const M<mod> &a, const M<mod> &b) {
-	M<mod> res = a;
+inline ModularArithmetic<mod> operator+(const ModularArithmetic<mod> &a, const ModularArithmetic<mod> &b) {
+	ModularArithmetic<mod> res = a;
 	res += b;
 	return res;
 }
 
 template <uint32_t mod>
-M<mod> operator-(const M<mod> &a, const M<mod> &b) {
-	M<mod> res = a;
+inline ModularArithmetic<mod> operator-(const ModularArithmetic<mod> &a, const ModularArithmetic<mod> &b) {
+	ModularArithmetic<mod> res = a;
 	res -= b;
 	return res;
 }
 
 template <uint32_t mod>
-M<mod> operator*(const M<mod> &a, const M<mod> &b) {
-	M<mod> res = a;
+inline ModularArithmetic<mod> operator*(const ModularArithmetic<mod> &a, const ModularArithmetic<mod> &b) {
+	ModularArithmetic<mod> res = a;
 	res *= b;
 	return res;
 }
 
 template <uint32_t mod>
-M<mod> operator/(const M<mod> &a, const M<mod> &b) {
-	M<mod> res = a;
+inline ModularArithmetic<mod> operator/(const ModularArithmetic<mod> &a, const ModularArithmetic<mod> &b) {
+	ModularArithmetic<mod> res = a;
 	res /= b;
 	return res;
 }
 
 template <uint32_t mod>
-M<mod> pow(M<mod> a, uint64_t d) {
-	M<mod> res = (d & 1u) ? a : M<mod>(1);
+inline ModularArithmetic<mod> pow(ModularArithmetic<mod> a, uint64_t d) {
+	ModularArithmetic<mod> res = (d & 1u) ? a : ModularArithmetic<mod>(1);
 	d >>= 1;
 	while(d) {
 		a *= a;
@@ -101,14 +98,14 @@ M<mod> pow(M<mod> a, uint64_t d) {
 }
 
 template <uint32_t mod>
-std::istream& operator>>(std::istream &in, M<mod> &a) {
+inline std::istream& operator>>(std::istream &in, ModularArithmetic<mod> &a) {
 	long long value;
 	in >> value;
-	a = M<mod>(value);
+	a = ModularArithmetic<mod>(value);
 	return in;
 }
 
 template <uint32_t mod>
-std::ostream& operator<<(std::ostream &out, const M<mod> &a) {
+inline std::ostream& operator<<(std::ostream &out, const ModularArithmetic<mod> &a) {
 	return out << a.get_value();
 }
